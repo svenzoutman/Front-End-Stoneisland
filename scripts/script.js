d62235f9916f577ae3687ff35304692bd6d5e683
Sven <sven.zoutman@gmail.com>
2020-10-05 13:36:10 +0200

    3

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,10 @@
 // JavaScript Document
+
+var deButton = document.querySelector("hamburger-menu");
+
+function doeFormNeerEnOp(){
+	let hetFormulier = document.querySelector("menu");
+	hetFormulier.classList.toggle("menu");
+}
+
+deButton.addEventListener("click", doeFormNeerEnOp);
